fe063ccf04fbbb07cb7cf2401e935987166321b3
Andrea <chela355@hotmail.com>
2021-02-09 18:28:13 -0500

    se hizo el ejercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace circulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Radio= 5;
+
+            double area= Math.PI * Math.Pow(Radio, 2);
+            double perimetro= (Math.PI * 2) * Radio;
+
+
+            Console.WriteLine();
+        }
+    }
+}
